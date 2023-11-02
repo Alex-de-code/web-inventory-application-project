e2084dfb45745100648058fb5705eccd3a4b8476
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-11-02 12:45:55 -0400

    rethinking NFT generator and will build basic  functionality of app before experimenting with npm package that lacks good documentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-//grabbed the form
+// grabbed the form
 const form = document.querySelector("form");
 
 form.addEventListener('submit', (event) => {
     //this will prevent the page from reloading so we don't lose data
     event.preventDefault();
     // get target data
-    const { name, id, cryptoCurrencies, price, inStock } = event.target;
+    const { name, id, cryptoCurrencies, price, inStock } = event.target.elements;
 
 })
+
+console.log("test..from main");
